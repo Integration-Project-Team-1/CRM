7a9c78effbdc73af4dc94bccea8292c23ea56873
Hamza <hamza.amghar@student.ehb.be>
2024-05-24 15:36:13 +0200

    Debug statment and xml validation test finished

--- src/main/java/crm/xmlValidation.java
+++ src/main/java/crm/xmlValidation.java
@@ -19,11 +19,15 @@ public class xmlValidation {
 
         String xsdPath = "src/main/resources/include.template.xsd";
         try {
+            System.out.println("Creating SchemaFactory instance."); //debug
             SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI); //instance of schemafactory for xml validation
+            System.out.println("Loading schema from file: " + xsdPath); //debug
             Schema schema = factory.newSchema(new File(xsdPath)); //instance of schema by parsing the xsd file
 
             Validator validator=schema.newValidator();
+            System.out.println("Validating XML against the schema.");  // Debug
             validator.validate(new StreamSource(new StringReader(xml))); //validating the xml against the xsd using streamsource object created from stringreader containing the xml
+            System.out.println("XML is valid.");  // Debug statement
         }catch (IOException | SAXException e){
             System.out.println("XML validation failed" + e.getMessage());
             return false;
